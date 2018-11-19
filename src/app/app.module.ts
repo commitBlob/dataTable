6152bb9d723e5f9b83dab12431590a8d84d186a0
Maro <Maro.Radovic@smithsnews.co.uk>
2018-11-19 15:46:22 +0000

    feat(Hammerjs): Add hammerjs dependency to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+// Core
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// App specific
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+// External
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
